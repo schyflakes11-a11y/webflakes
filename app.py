bfa30ae7964b8c1a471a88e0ae2cb6b6c7cc0b47
schyflakes11-a11y <schyflakes11@gmail.com>
2026-04-30 20:24:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import *
+import random
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/random-number-generator", methods=["GET", "POST"])
+def rannum():
+    number = None
+    if request.method == "POST":
+        number = random.randint(1, 100)
+    return render_template("randomnumbergenerator.html", number=number)
+        
 if __name__ == "__main__":
     app.run()
